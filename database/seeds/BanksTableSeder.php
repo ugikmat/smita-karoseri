5be52b4a5f3bc84ee227053d61b5bff17760240b
ugikmat <ugik.mats@gmail.com>
2018-07-14 08:42:46 +0700

    Add Bank Seeder

---
+++ database/seeds/BanksTableSeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BanksTableSeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $bca = new Bank();
+        $bca->nama='BCA';
+        $bca->save();
+
+        $bri = new Bank();
+        $bri->nama='BRI';
+        $bri->save();
+
+        $bni = new Bank();
+        $bni->nama='BNI';
+        $bni->save();
+
+        $mandiri = new Bank();
+        $mandiri->nama='Mandiri';
+        $mandiri->save();
+    }
+}
