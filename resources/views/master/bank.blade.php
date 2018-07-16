b7fbcfc3274b1032677e656fd5d65738bc0c7c96
andichapras <prassmile.ps@gmail.com>
2018-07-16 16:11:51 +0700

    bagian dwizta

--- resources/views/master/bank.blade.php
+++ resources/views/master/bank.blade.php
@@ -18,127 +18,70 @@
     </thead>
 </table>
 <!-- Button to Open the Modal -->
-<section class="content-header">
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal1">Tambah</button>
-      <div class="modal fade bs-example-modal-lg" id='modal1' tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-              </button>
-              <h4 class="modal-title" id="myModalLabel">Tambah Bank</h4>
-            </div>
-            <div class="modal-body">
-               <div class="clearfix"></div>
-<div class="row">
-  <div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel">
-      <div class="x_title">
-        <div class="clearfix"></div>
-      </div>
-      <div class="x_content">
-        <br />
-
-        <form id="tambah-lokasi" method="post" data-parsley-validate class="form-horizontal form-label-left" action="">
-           <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Id Bank<span class="required">*</span>
-            </label>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-              <input type="text" id="first-name" required="required" name="idcs" class="form-control col-md-7 col-xs-12" value="">
-            </div>
-          </div>
-
-          <div class="form-group">
-           <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nama Bank<span class="required">*</span>
-           </label>
-           <div class="col-md-6 col-sm-6 col-xs-12">
-             <input type="text" id="first-name" required="required" name="nama" class="form-control col-md-7 col-xs-12" value="">
-           </div>
-         </div>
-
-          <div class="ln_solid"></div>
-          <div class="form-group">
-            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-  <button class="btn btn-primary" type="reset">Reset</button>
-              <button type="submit" class="btn btn-success">Submit</button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-
-          </div>
-        </div>
-      </div>
-      </section>
-
-
-<!--Modal Edit-->
-<div class="modal fade bs-example-modal-lg" id='modal1' tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-lg">
+<button type="button" class="btn btn-primary btn-flat align-right" data-toggle="modal" data-target="#myModal">
+    Tambah
+</button>
+<!--Modal Tambah-->
+<div class="modal fade" id="myModal">
+  <div class="modal-dialog">
     <div class="modal-content">
 
+      <!-- Modal Header -->
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
-        </button>
-        <h4 class="modal-title" id="myModalLabel">Edit Bank</h4>
+        <h4 class="modal-title">Tambah Bank</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
+
+      <!-- Modal body -->
       <div class="modal-body">
-         <div class="clearfix"></div>
-<div class="row">
-<div class="col-md-12 col-sm-12 col-xs-12">
-<div class="x_panel">
-<div class="x_title">
-  <div class="clearfix"></div>
-</div>
-<div class="x_content">
-  <br />
-
-  <form id="tambah-lokasi" method="post" data-parsley-validate class="form-horizontal form-label-left" action="">
-     <div class="form-group">
-      <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Id Bank<span class="required">*</span>
-      </label>
-      <div class="col-md-6 col-sm-6 col-xs-12">
-        <input type="text" id="first-name" required="required" name="idcs" class="form-control col-md-7 col-xs-12" value="">
+        <div class="input-group">
+            <span class="input-group-addon">
+                <i class="fa fa-bank"></i>
+            </span>
+            <input id="nama" name="nama" type="text" class="form-control" placeholder="Bank Name">
+        </div>
       </div>
-    </div>
 
-    <div class="form-group">
-     <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nama Bank<span class="required">*</span>
-     </label>
-     <div class="col-md-6 col-sm-6 col-xs-12">
-       <input type="text" id="first-name" required="required" name="nama" class="form-control col-md-7 col-xs-12" value="">
-     </div>
-   </div>
-
-    <div class="ln_solid"></div>
-    <div class="form-group">
-      <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
-<button class="btn btn-primary" type="reset">Reset</button>
-        <button type="submit" class="btn btn-success">Submit</button>
-      </div>
-    </div>
-  </form>
-</div>
-</div>
-</div>
-</div>
-</div>
+      <!-- Modal footer -->
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Tambah</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
       </div>
 
     </div>
   </div>
 </div>
 
+<!--Modal Tambah-->
+<div class="modal fade" id="editModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">Edit Bank</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        <div class="input-group">
+            <span class="input-group-addon">
+                <i class="fa fa-bank"></i>
+            </span>
+            <input id="nama" name="nama" type="text" class="form-control" placeholder="Bank Name">
+        </div>
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Simpan</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+      </div>
+
+    </div>
+  </div>
+</div>
 
 <!--Modal Hapus-->
 <div class="modal fade" id="deleteModal">
