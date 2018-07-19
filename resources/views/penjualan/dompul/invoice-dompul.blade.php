05ca9dc5ff1bbe3f946cbcf7c0b603a3aa752af2
andichapras <prassmile.ps@gmail.com>
2018-07-19 13:26:35 +0700

    add file for transaction

--- resources/views/penjualan/dompul/invoice-dompul.blade.php
+++ resources/views/penjualan/dompul/invoice-dompul.blade.php
@@ -6,27 +6,34 @@
     <h1>Penjualan Dompul</h1>
 @stop
 
+@section('css')
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/u/bs-3.3.6/jq-2.2.3,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.0,b-colvis-1.2.0,b-html5-1.2.0,b-print-1.2.0,fh-3.1.2,se-1.2.0/datatables.min.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/css/bootstrap-datepicker.min.css"/>
+@stop
+
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class='col-sm-6'>
-            <div class="form-group">
-                <div class='input-group date' id='datetimepicker1'>
-                    <input type='text' class="form-control" />
-                    <span class="input-group-addon">
-                        <span class="glyphicon glyphicon-calendar"></span>
-                    </span>
-                </div>
-            </div>
-        </div>
-        <script type="text/javascript">
-            $(function () {
-                $('#datetimepicker1').datetimepicker();
-            });
-        </script>
+<form class="form-horizontal">
+    <div class="form-group form-group-sm">
+      <label class="col-sm-2 control-label" for="lg">ID canvaser</label>
+      <div class="col-sm-4">
+        <input class="form-control" type="text" id="sm" placeholder="masukkan ID canvaser">
+      </div>
     </div>
-</div>
-<table id="users-table" class="table table-bordered">
+    <div class="form-group form-group-sm">
+      <label class="col-sm-2 control-label" for="sm">No Penjualan</label>
+      <div class="col-sm-4">
+        <input class="form-control" type="text" id="sm" placeholder="masukkan no penjualan">
+      </div>
+    </div>
+    <div class="form-group form-group-sm">
+      <label class="col-sm-2 control-label" for="sm">Tgl Penjualan</label>
+      <div class="col-sm-4" date_picker>
+          <input type="text" id="update_time" class="form-control" name="update_time" placeholder="Pick Date">
+      </div>
+    </div>
+  </form>
+
+<!-- <table>
     <tr>
       <td>ID canvaser</td>
       <td>:</td>
@@ -47,21 +54,17 @@
       <td>Tgl Penjualan</td>
       <td>:</td>
       <td>
-        <div class="input-append date" id="dp3" data-date="" data-date-format="">
-          <input class="span2" size="16" type="text" value="">
+        <div class="col-md-10 col-sm-6 col-xs-8 date_picker">
+          <input type="text" id="update_time" class="form-control" name="update_time" placeholder="Pick Date">
         </div>
       </td>
     </tr>
-    <input type="button" name="" value="Kosongkan">
-    <input type="button" name="" value="Rekap Penjualan">
-</table>
+</table> -->
+<button class="btn btn-primary" type="reset">Kosongkan</button>
+<button type="submit" class="btn btn-success">Rekap Penjualan</button>
 @stop
 
 @section('js')
-<script src="js/google-code-prettify/prettify.js"></script>
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap-datepicker.js"></script>
-
 <script>
     $(function () {
         $('#users-table').DataTable({
@@ -81,4 +84,14 @@
         });
     });
 </script>
+<script type="text/javascript" src="https://cdn.datatables.net/u/bs-3.3.6/jq-2.2.3,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.12,b-1.2.0,b-colvis-1.2.0,b-html5-1.2.0,b-print-1.2.0,fh-3.1.2,se-1.2.0/datatables.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/js/bootstrap-datepicker.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.1/locales/bootstrap-datepicker.en.min.js"></script>
+<script type="text/javascript">
+        $('.date_picker input').datepicker({
+          format: "dd.mm.yyyy",
+          todayBtn: "linked",
+          language: "en"
+        });
+    </script>
 @stop
