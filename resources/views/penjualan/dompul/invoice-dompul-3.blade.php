61715e2c80c80ee9044efa6b57d14f3694d6763e
andichapras <prassmile.ps@gmail.com>
2018-07-25 11:06:39 +0700

    update invoice dompul

--- resources/views/penjualan/dompul/invoice-dompul-3.blade.php
+++ resources/views/penjualan/dompul/invoice-dompul-3.blade.php
@@ -62,7 +62,93 @@
         <th>Action</th>
     </tr>
     </thead>
+    <tfoot>
+      <tr>
+        <th></th>
+        <th>Grand Total</th>
+        <th></th>
+        <th>Harga Total</th>
+      </tr>
+    </tfoot>
 </table>
+
+<!--Modal Edit-->
+<div class="modal fade bs-example-modal-lg" id='editModal' tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">
+          <span aria-hidden="true">×</span>
+        </button>
+        <h4 class="modal-title" id="myModalLabel">Edit Tipe</h4>
+      </div>
+      <div class="modal-body">
+        <div class="clearfix"></div>
+        <div class="row">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+              <div class="x_title">
+                <div class="clearfix"></div>
+              </div>
+              <div class="x_content">
+                <br />
+
+                <form id="editForm" method="POST" data-parsley-validate class="form-horizontal form-label-left" action="">
+                  @csrf @method('put')
+                  <div class="form-group kode">
+                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Tipe Dompul
+                      <span class="required">*</span>
+                    </label>
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                      <select name="tipe" required="required">
+                        <option value="selected" selected>-- Pilih Tipe Dompul --</option>
+                        <option value="ds">DS</option>
+                        <option value="cvs">CVS</option>
+                        <option value="hi">HI</option>
+                        <option value="server">SERVER</option>
+                        <option value="sde">SDE</option>
+                        <option value="cvs1">CVS1</option>
+                        <option value="cvs2">CVS2</option>
+                        <option value="cvs3">CVS3</option>
+                        <option value="server1">SERVER1</option>
+                        <option value="server2">SERVER2</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div class="form-group nama">
+                    <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Qty Program
+                      <span class="required">*</span>
+                    </label>
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input type="text" id="first-name" required="required" name="nama" class="form-control col-md-7 col-xs-12" value="Auto Generate"  disabled>
+                    </div>
+                  </div>
+
+
+                  <div class="ln_solid"></div>
+                  <div class="form-group">
+                    <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                      <button class="btn btn-primary" type="reset">Reset</button>
+                      <input type="submit" class="btn btn-success" value="Simpan">
+                      {{-- <button type="button" class="btn btn-primary" data-dismiss="modal"> Simpan</button> --}}
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+
 @stop
 
 @section('js')
