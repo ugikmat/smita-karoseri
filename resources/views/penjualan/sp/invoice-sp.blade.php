5e4efebe0eb645572731c7726d2f3d8eece0e942
andichapras <prassmile.ps@gmail.com>
2018-08-05 11:07:19 +0700

    data repeater in invoice sp

--- resources/views/penjualan/sp/invoice-sp.blade.php
+++ resources/views/penjualan/sp/invoice-sp.blade.php
@@ -88,6 +88,26 @@
   </div>
 </div>
 
+<form class="repeater">
+    <!--
+        The value given to the data-repeater-list attribute will be used as the
+        base of rewritten name attributes.  In this example, the first
+        data-repeater-item's name attribute would become group-a[0][text-input],
+        and the second data-repeater-item would become group-a[1][text-input]
+    -->
+    <div data-repeater-list="group-a">
+      <div data-repeater-item>
+        <input type="text" name="text-input" value="A"/>
+        <input data-repeater-delete type="button" value="Delete"/>
+      </div>
+      <div data-repeater-item>
+        <input type="text" name="text-input" value="B"/>
+        <input data-repeater-delete type="button" value="Delete"/>
+      </div>
+    </div>
+    <button data-repeater-create type="button" class="btn btn-success"> <span class="glyphicon glyphicon-plus"></span> Add</button>
+</form>
+
 <!-- <table id="invoice-sp-table" class="table responsive" width="100%">
     <thead>
     <tr>
@@ -192,6 +212,56 @@
 
 @section('js')
 <script>
+    $(document).ready(function () {
+        $('.repeater').repeater({
+            // (Optional)
+            // start with an empty list of repeaters. Set your first (and only)
+            // "data-repeater-item" with style="display:none;" and pass the
+            // following configuration flag
+            initEmpty: true,
+            // (Optional)
+            // "defaultValues" sets the values of added items.  The keys of
+            // defaultValues refer to the value of the input's name attribute.
+            // If a default value is not specified for an input, then it will
+            // have its value cleared.
+            defaultValues: {
+                'text-input': 'foo'
+            },
+            // (Optional)
+            // "show" is called just after an item is added.  The item is hidden
+            // at this point.  If a show callback is not given the item will
+            // have $(this).show() called on it.
+            show: function () {
+                $(this).slideDown();
+            },
+            // (Optional)
+            // "hide" is called when a user clicks on a data-repeater-delete
+            // element.  The item is still visible.  "hide" is passed a function
+            // as its first argument which will properly remove the item.
+            // "hide" allows for a confirmation step, to send a delete request
+            // to the server, etc.  If a hide callback is not given the item
+            // will be deleted.
+            hide: function (deleteElement) {
+                if(confirm('Apakah anda yakin ingin menghapus pesanan SP ini?')) {
+                    $(this).slideUp(deleteElement);
+                }
+            },
+            // (Optional)
+            // You can use this if you need to manually re-index the list
+            // for example if you are using a drag and drop library to reorder
+            // list items.
+            // ready: function (setIndexes) {
+            //     $dragAndDrop.on('drop', setIndexes);
+            // },
+            // (Optional)
+            // Removes the delete button from the first list item,
+            // defaults to false.
+            isFirstItemUndeletable: true
+        })
+    });
+</script>
+
+<!-- <script>
     $(function () {
         if ($('#tgl').val()==undefined) {
           var tgl = null;
@@ -236,7 +306,7 @@
           } );
         } ).draw();
     });
-</script>
+</script> -->
 <script src="{{ asset('/datepicker/js/bootstrap-datepicker.min.js') }}"></script>
 <script>
 $('.datepicker').datepicker({
