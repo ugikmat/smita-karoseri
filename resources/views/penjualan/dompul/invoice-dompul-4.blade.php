ea1d456867a0eba6db1c81269d92cbdf4777a857
andichapras <prassmile.ps@gmail.com>
2018-08-14 14:37:54 +0700

    invoice dompu 4 done revisiannya, dibuat data repeater

--- resources/views/penjualan/dompul/invoice-dompul-4.blade.php
+++ resources/views/penjualan/dompul/invoice-dompul-4.blade.php
@@ -45,7 +45,7 @@ td{
     </div>
   </div>
 
-<form class="invoice-dompul" action="/invoice_dompul/store" method="post">
+<form class="invoice-dompul repeater" action="/invoice_dompul/store" method="post">
   @csrf
   <input type="hidden" name="sales" id="sales" value="{{$sales->id_sales}}">
   <input type="hidden" name="nm_sales" id="nm_sales" value="{{$sales->nm_sales}}">
@@ -64,59 +64,136 @@ td{
         <th>Harga Total</th>
     </tr>
     </thead>
-      <tfoot>
-        <tr>
-          <td></td>
-          <td></td>
-          <td colspan="2"><b>Grand Total</b></td>
-          <td></td>
-          <td>
-            @isset($total)
-            <input type="text" name="total" id="total" value="{{$total}}" readonly>
-            @endisset
-          </td>
-        </tr>
-        @foreach($bank as $key=>$value)
-        <tr>
-          <td></td>
-          <td></td>
-          <td colspan="2"><b>Pembayaran {{$key+1}}</b></td>
-          <td></td>
-        <td><input type="text" id="bank{{$key}}" required="required" name="bank[{{$key}}][bank]" class="form-control" value="{{$value['bank']}}" readonly></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td colspan="2"><b>Nominal {{$key+1}}</b></td>
-          <td></td>
-          <td><input type="text" id="trf{{$key}}" required="required" name="bank[{{$key}}][trf]" class="form-control" value="{{$value['trf']}}" readonly></td>
-
-        </tr>
-        <tr>
-          <td></td>
-          <td></td>
-          <td colspan="2"><b>Catatan {{$key+1}}</b></td>
-          <td></td>
-          <td><input type="text" id="catatan{{$key}}" required="required" name="bank[{{$key}}][catatan]" class="form-control" value="{{$value['catatan']}}" readonly></td>
-
-        </tr>
-        @endforeach
-        <tr>
-          <td colspan="6">
-            <div class="pull-right">
-              <button type="button" onclick="goBack()" class="btn btn-danger"><span class="glyphicon glyphicon-chevron-left"></span> Kembali</button>
-              <button type="submit" class="btn btn-success" name="button"><span class="glyphicon glyphicon-ok"></span> Simpan</button>
-            </div>
-          </td>
-        </tr>
-    </tfoot>
 </table>
+<br>
+<div class="container-fluid" style="background:white;">
+  <br>
+  <div class="form row">
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <b>Jumlah Tunai</b>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      @isset($total)
+      <input type="text" class="form-control" name="total" id="total" value="{{$total}}" readonly>
+      @endisset
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+  </div>
+  <hr>
+  <div data-repeater-list="bank-sp">
+    <div data-repeater-item>
+      <div class="form row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3">
+          <b>Pembayaran</b>
+          <br>
+          <input type="text" id="bank" name="bank" class="form-control" value="" readonly>
+        </div>
+        <div class="col-xs-5 col-sm-5 col-md-5 col-lg-3">
+          <b>Nominal</b>
+          <br>
+          <input type="text" id="trf" name="trf" class="form-control" value="" readonly>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <b>Catatan</b>
+          <br>
+          <input type="text" id="catatan" name="catatan" class="form-control" value="" readonly>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <br>
+        </div>
+      </div>
+    <hr>
+    </div>
+  </div>
+
+<div class="row">
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+  </div>
+  <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+  </div>
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+  </div>
+  <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+    <br>
+    <button type="button" onclick="goBack()" class="btn btn-danger"><span class="glyphicon glyphicon-chevron-left"></span> Kembali</button>
+    <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Simpan</button>
+    <br><br>
+  </div>
+</div>
+</div>
+
 </form>
 
 @stop
 
 @section('js')
+<script>
+    $(document).ready(function () {
+        var repeater = $('.repeater').repeater({
+            // (Optional)
+            // start with an empty list of repeaters. Set your first (and only)
+            // "data-repeater-item" with style="display:none;" and pass the
+            // following configuration flag
+            // initEmpty: true,
+            // (Optional)
+            // "defaultValues" sets the values of added items.  The keys of
+            // defaultValues refer to the value of the input's name attribute.
+            // If a default value is not specified for an input, then it will
+            // have its value cleared.
+            // defaultValues: {
+            //     'text-input': 'foo'
+            // },
+            // (Optional)
+            // "show" is called just after an item is added.  The item is hidden
+            // at this point.  If a show callback is not given the item will
+            // have $(this).show() called on it.
+            show: function () {
+                $(this).slideDown();
+            },
+            // (Optional)
+            // "hide" is called when a user clicks on a data-repeater-delete
+            // element.  The item is still visible.  "hide" is passed a function
+            // as its first argument which will properly remove the item.
+            // "hide" allows for a confirmation step, to send a delete request
+            // to the server, etc.  If a hide callback is not given the item
+            // will be deleted.
+            hide: function (deleteElement) {
+                if(confirm('Are you sure you want to delete this element?')) {
+                    $(this).slideUp(deleteElement);
+                }
+            },
+            // (Optional)
+            // You can use this if you need to manually re-index the list
+            // for example if you are using a drag and drop library to reorder
+            // list items.
+            // ready: function (setIndexes) {
+            //     $dragAndDrop.on('drop', setIndexes);
+            // },
+            // (Optional)
+            // Removes the delete button from the first list item,
+            // defaults to false.
+            isFirstItemUndeletable: false
+        });
+        repeater.setList([
+          @foreach($bank as $item)
+          {
+                'bank': "{{$item['bank']}}",
+                'trf' : "{{$item['trf']}}",
+                'catatan' : "{{$item['catatan']}}"
+            },
+          @endforeach
+        ]);
+    });
+</script>
+
 <script>
 function goBack() {
     window.history.back()
