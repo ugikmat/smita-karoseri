fd272e3afcff1a3e1ff7596d076c4ff6f0501be6
andichapras <prassmile.ps@gmail.com>
2018-08-20 13:45:39 +0700

    add modal delete in list invoice dompul

--- resources/views/penjualan/dompul/list-edit-p-dompul-ro.blade.php
+++ resources/views/penjualan/dompul/list-edit-p-dompul-ro.blade.php
@@ -316,6 +316,27 @@
   </div>
 </div>
 
+<!--Modal Hapus-->
+<div class="modal fade" id="deleteModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <form id="deleteForm" action="" method="POST">
+        @csrf @method('delete')
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="modal-title">Apakah Anda Yakin ingin menghapus?</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <input type="submit" class="btn btn-danger" value="Hapus">
+          <button type="button" class="btn btn-primary" data-dismiss="modal">Tutup</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
 @stop
 
 @section('js')
