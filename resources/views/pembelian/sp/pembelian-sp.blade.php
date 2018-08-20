2154c0e8ce2930bbcc0c4f665142d153ca73f13a
andichapras <prassmile.ps@gmail.com>
2018-08-20 11:28:37 +0700

    add pembelian sp

---
+++ resources/views/pembelian/sp/pembelian-sp.blade.php
@@ -0,0 +1,354 @@
+@extends('adminlte::page')
+
+@section('title', 'Pembelian SP')
+
+@section('content_header')
+    <h1>Pembelian SP</h1>
+@stop
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('/datepicker/css/bootstrap-datepicker.min.css') }}">
+<style>
+  #kiri{
+    padding-left: 0px;
+  }
+</style>
+@stop
+
+@section('content')
+<form class="invoice-sp repeater" action="/invoice_sp/verify" method="post">
+<div class="container-fluid  form-inline">
+  @csrf
+  <div class="row">
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4" id="kiri">
+      Tanggal Penjualan : &nbsp;
+      <input class="datepicker form-control" data-date-format="dd-mm-yyyy" id="tgl_penjualan" name="tgl_penjualan" value="">
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4" id="kiri">
+        Nama Canvaser : &nbsp;
+        <select id="sales" required="required" name="sales" class="chosen-select" data-placeholder="">
+              <option value="" disabled>Pilih Nama Canvaser</option>
+                  <option value=""></option>
+        </select>
+    </div>
+    <div class="col-xs-6 col-sm-6 col-md-4 col-lg-4" id="kiri">
+        Nama Kios : &nbsp;
+        <select id="customer" required="required" name="customer" placeholder="Pilih Nama Kios" class="chosen-select" data-placeholder="">
+              <option value="" disabled>Pilih Nama Kios</option>
+                  <option value=""></option>
+        </select>
+    </div>
+  </div>
+</div>
+
+<br><br>
+
+<table id="invoice-sp-table" class="table responsive"  width="100%">
+  <tr>
+    <th>Nama Barang</th>
+    <th>Satuan</th>
+    <th>Harga Satuan</th>
+    <th>Tipe Harga</th>
+    <th>Jumlah</th>
+    <th>Harga Total</th>
+  </tr>
+
+  <tr>
+    <input type="hidden" name="kode" id="kode" value="">
+    <td>
+    <input type="text" class="form-control" id="nama" name="nama" value="" disabled>
+    </td>
+    <td>
+      <input type="text" class="form-control" id="satuan" name="satuan" value="" disabled>
+    </td>
+    <td>
+      <input type="text" class="form-control" id="harga" name="harga" value="" readonly>
+    </td>
+    <td>
+      <select class="form-control" name="tipe" id="tipe" style="height: calc(3.5rem - 2px); width:100%;">
+            <option  value=""></option>
+      </select>
+    </td>
+    <td>
+      <input type="number" class="form-control" id="jumlah" name="jumlah" value=0 style="width=100%;">
+    </td>
+    <td>
+      <input type="text" class="form-control" id="total" name="total" readonly value=0>
+    </td>
+  </tr>
+</table>
+
+<div class="container-fluid" style="background:white;">
+  <br>
+  <div class="form row">
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <b>Jumlah Tunai</b>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <input type="text" class="form-control" name="total" id="total" value="" readonly>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+  </div>
+  <div class="form row">
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+      <b>Total Pembayaran</b>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+        <input type="text" class="form-control" name="pembayaran" id="total_pembayaran" value="" readonly>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+</div>
+  <hr>
+  <div data-repeater-list="bank-sp" id="pembayaran">
+    <div data-repeater-item>
+      <div class="form row">
+        <div class="col-xs-2 col-sm-2 col-md-2 col-lg-3">
+          <b>Pembayaran</b>
+          <br>
+          <select name="bank" id="bank" style="height: calc(3.5rem - 2px); width:100%;" required="required">
+            <option value="">-- Cara Pembayaran --</option>
+            <option value="Cash">Cash</option>
+            <option value="BCA Pusat">BCA Pusat</option>
+            <option value="BCA Cabang">BCA Cabang</option>
+            <option value="BRI">BRI</option>
+            <option value="BNI">BNI</option>
+            <option value="Mandiri">Mandiri</option>
+          </select>
+        </div>
+        <div class="col-xs-5 col-sm-5 col-md-5 col-lg-3">
+          <b>Nominal</b>
+          <br>
+          <input type="text" id="trf" name="trf" class="form-control" value="" required="required" autocomplete="off">
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <b>Catatan</b>
+          <br>
+          <input type="text" id="catatan" name="catatan" class="form-control" value="" autocomplete="off">
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+          <br>
+          <button data-repeater-delete type="button" class="btn btn-danger"> <span class="glyphicon glyphicon-remove"></span> Delete</button>
+        </div>
+      </div>
+    <hr>
+    </div>
+  </div>
+<button data-repeater-create type="button" class="btn btn-warning"> <span class="glyphicon glyphicon-plus"></span> Tambah Pembayaran</button>
+
+<div class="row">
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+  </div>
+  <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
+  </div>
+  <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+
+  </div>
+  <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+    <br>
+    <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-ok"></span> Nota Penjualan</button>
+    <br><br>
+  </div>
+</div>
+</div>
+
+</form>
+<!-- <table id="invoice-sp-table" class="table responsive" width="100%">
+    <thead>
+    <tr>
+        <th>No.</th>
+        <th>Nama RO</th>
+        <th>Qty Penjualan</th>
+    </tr>
+    </thead>
+</table> -->
+
+@stop
+@section('js')
+<script>
+    $(document).ready(function () {
+        $('.repeater').repeater({
+            // (Optional)
+            // start with an empty list of repeaters. Set your first (and only)
+            // "data-repeater-item" with style="display:none;" and pass the
+            // following configuration flag
+            // initEmpty: true,
+            // (Optional)
+            // "defaultValues" sets the values of added items.  The keys of
+            // defaultValues refer to the value of the input's name attribute.
+            // If a default value is not specified for an input, then it will
+            // have its value cleared.
+            // defaultValues: {
+            //     'text-input': 'foo'
+            // },
+            // (Optional)
+            // "show" is called just after an item is added.  The item is hidden
+            // at this point.  If a show callback is not given the item will
+            // have $(this).show() called on it.
+            show: function () {
+                $(this).slideDown();
+            },
+            // (Optional)
+            // "hide" is called when a user clicks on a data-repeater-delete
+            // element.  The item is still visible.  "hide" is passed a function
+            // as its first argument which will properly remove the item.
+            // "hide" allows for a confirmation step, to send a delete request
+            // to the server, etc.  If a hide callback is not given the item
+            // will be deleted.
+            hide: function (deleteElement) {
+                if(confirm('Are you sure you want to delete this element?')) {
+                    $(this).slideUp(deleteElement);
+                }
+            },
+            // (Optional)
+            // You can use this if you need to manually re-index the list
+            // for example if you are using a drag and drop library to reorder
+            // list items.
+            // ready: function (setIndexes) {
+            //     $dragAndDrop.on('drop', setIndexes);
+            // },
+            // (Optional)
+            // Removes the delete button from the first list item,
+            // defaults to false.
+            isFirstItemUndeletable: false
+        });
+        // $("#pembayaran").on("keyup", "#trf", function(){
+        //   if (this.value.length!=0) {
+        //     var n = parseInt($(this).val().replace(/\D/g,''),10);
+        //     (this).value=n.toLocaleString('id-ID');
+        //   }else{
+        //     (this).value=0;
+        //   }
+        // });
+    });
+</script>
+<script type="text/javascript">
+  $(".chosen-select").chosen();
+  // $("#sales").val("{{session('id_sales')}}");
+  // $("#customer").val("{{session('id_cust')}}");
+  // $("#sales").trigger("chosen:updated");
+  // $("#customer").trigger("chosen:updated");
+</script>
+<script type="text/javascript">
+// $.ajaxSetup({
+//     headers: {
+//         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//     }
+// });
+// var harga = [];
+// var totalHarga = 0;
+// for (let index = 0; index < {{$jumlah}}; index++) {
+//   harga.push($(`#total${index+1}`).val().replace(/[ .]/g, ''));
+//   totalHarga+=parseFloat(harga[index]);
+//   console.log(`total Harga ${$(`#total${index+1}`).val().replace(/[ .]/g, '')}`);
+//   $(`#jumlah${index+1}`).on('input',function (event) {
+//     totalHarga-=parseFloat(harga[index]);
+//     $(`#total${index+1}`).val(($(`#harga${index+1}`).val().replace(/[ .]/g, '')*this.value.replace(/[ .]/g, '')).toLocaleString('id-ID'));
+//     harga[index]=$(`#total${index+1}`).val().replace(/[ .]/g, '');
+//     totalHarga+=parseFloat(harga[index]);
+//     console.log(`total Harga ${harga[index]}`);
+//     $('#total').val(totalHarga.toLocaleString('id-ID'));
+//   });
+  // $(`#tipe${index+1}`).on('change',function (event) {
+  //   $.session.set("tipe_harga", this.value);
+  //   $.session.set("kode_produk", $(`#kode${index+1}`).val());
+  //   $(`#harga${index+1}`).val({{$hargaProduks->where('id_produk',session('kode_produk'))->where('tipe_harga_sp',session('tipe_harga'))->first()['harga_sp']}});
+  // });
+// }
+// $('#total').val(totalHarga.toLocaleString('id-ID'));
+// console.log(`total Harga`);
+// for (let index = 0; index < {{$jumlah}}; index++) {
+//
+//   $(`#tipe${index+1}`).on('change',function (event) {
+//     //ajax call
+//     $.post('/get_harga/'+$(this).val()+'/'+$(`#kode${index+1}`).val(), function(response){
+//     if(response.success)
+//     {
+//       totalHarga-=parseFloat(harga[index]);
+//       console.log(response.harga);
+//       $(`#harga${index+1}`).val(response.harga.harga_sp.toLocaleString('id-ID'));
+//       $(`#total${index+1}`).val(($(`#harga${index+1}`).val().replace(/[ .]/g, '')*$(`#jumlah${index+1}`).val().replace(/[ .]/g, '')).toLocaleString('id-ID'));
+//       harga[index]=$(`#total${index+1}`).val().replace(/[ .]/g, '');
+//       totalHarga+=parseFloat(harga[index]);
+//       console.log(`total Harga ${totalHarga}`);
+//       $('#total').val(totalHarga.toLocaleString('id-ID'));
+//     }
+// }, 'json');
+//   });
+// }
+  // $produks.forEach(function myFunction(item, index) {
+  //   console.log(index)
+  //   $(`#jumlah${index+1}`).on('keyup',function (event) {
+  //     $(`#total${index+1}`).val($(`#harga${index+1}`).val()*this.value);
+  // })
+  // })
+
+  // function today(){
+  //   var today = new Date();
+  //   var today = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  // }
+
+</script>
+<!-- <script>
+    $(function () {
+        if ($('#tgl').val()==undefined) {
+          var tgl = null;
+        } else {
+          var tgl = $('#tgl').val();
+        }
+        if ($('#canvaser').val()==undefined) {
+          var canvaser = null;
+        } else {
+          var canvaser = $('#canvaser').val();
+        }
+        var t = $('#invoice-sp-table').DataTable({
+            serverSide: true,
+            processing: true,
+            ajax: `/invoice_dompul/${canvaser}/${tgl}`,
+            "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+                } ],
+            "order": [[ 1, 'asc' ]],
+            columnDefs: [
+                {
+                    targets:1,
+                    render: function ( data, type, row, meta ) {
+                        if(type === 'display'){
+                            data = `<a class="link-post" href="/penjualan/dompul/${canvaser}/${tgl}/${data}">` + data + '</a>';
+                        }
+                        return data;
+                    }
+                }
+            ],
+            columns: [
+                {data: 'indeks'},
+                {data: 'nama_downline'},
+                {data: 'qty'}
+            ]
+        });
+        t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+          } );
+        } ).draw();
+    });
+</script> -->
+<script src="{{ asset('/datepicker/js/bootstrap-datepicker.min.js') }}"></script>
+<script>
+$('.datepicker').datepicker({
+  });
+</script>
+@stop
