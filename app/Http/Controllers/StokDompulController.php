fbfec17e74fb2f0762a5bf9db505edc4445cb328
ugikmat <ugik.mats@gmail.com>
2018-08-21 07:46:44 +0700

    Update Persediaan Dompul

--- app/Http/Controllers/StokDompulController.php
+++ app/Http/Controllers/StokDompulController.php
@@ -32,12 +32,14 @@ class StokDompulController extends Controller
     public function data(Datatables $datatables,$tgl)
     {
         // $data = DB::table('temp_detail_penjualan_sps')->get();
-        $tgl = Carbon::parse($tgl);
-        $tgl = $tgl->format('Y-m-d');
         session(['tgl_stok_dompul'=>$tgl]);
-        $stokDompul = DB::table('kartu_stok_dompuls')->select(DB::raw('kartu_stok_dompuls.id_produk,
+        $tgl = Carbon::parse($tgl);
+        $tgl = $tgl->format('Y-m-d');        
+        $stokDompul = DB::table('kartu_stok_dompuls')->select(DB::raw("kartu_stok_dompuls.id_produk,
+        (SELECT sum(awal.masuk)-sum(awal.keluar)
+FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi < '{$tgl}' AND awal.id_produk=kartu_stok_dompuls.id_produk) AS stok_awal,
 sum(kartu_stok_dompuls.masuk) AS stok_masuk, sum(kartu_stok_dompuls.keluar) AS stok_keluar,
-(sum(kartu_stok_dompuls.masuk)-sum(kartu_stok_dompuls.keluar)) AS jumlah_stok'))
+(sum(kartu_stok_dompuls.masuk)-sum(kartu_stok_dompuls.keluar)) AS jumlah_stok"))
                         // ->join('upload_dompuls',function($join){
                         //     $join->on('kartu_stok_dompuls.id_produk','=','upload_dompuls.produk');
                         // })
@@ -48,7 +50,7 @@ sum(kartu_stok_dompuls.masuk) AS stok_masuk, sum(kartu_stok_dompuls.keluar) AS s
                               return '';
                             })
                             ->addColumn('stok_awal', function ($dataStok) {
-                              return number_format(0,0,'','.');
+                              return number_format($dataStok->stok_awal,0,'','.');
                             })
                             ->addColumn('stok_masuk', function ($dataStok) {
                               return number_format($dataStok->stok_masuk,0,'','.');
