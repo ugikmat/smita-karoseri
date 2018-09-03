b3b01c9e5aca261b3657549a938582bc6b3f9f7e
ugikmat <ugik.mats@gmail.com>
2018-09-03 09:37:44 +0700

    Update mutasi dompul

--- app/Http/Controllers/StokDompulController.php
+++ app/Http/Controllers/StokDompulController.php
@@ -39,18 +39,28 @@ class StokDompulController extends Controller
         
         $tgl_akhir = Carbon::parse($tgl_akhir);
         $tgl_akhir = $tgl_akhir->format('Y-m-d');  
+//         $stokDompul = DB::table('kartu_stok_dompuls')->select(DB::raw("kartu_stok_dompuls.id_produk as nama,
+//         (SELECT sum(awal.masuk)-sum(awal.keluar)
+// FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi < '{$tgl_awal}' AND awal.id_produk=nama) AS stok_awal,
+// sum(kartu_stok_dompuls.masuk) AS stok_masuk, 
+// sum(kartu_stok_dompuls.keluar) AS stok_keluar,
+// (sum(kartu_stok_dompuls.masuk)-sum(kartu_stok_dompuls.keluar)+COALESCE((SELECT sum(awal.masuk)-sum(awal.keluar)
+// FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi < '{$tgl_awal}' AND awal.id_produk=nama),0)) AS jumlah_stok"))
+//                         // ->join('upload_dompuls',function($join){
+//                         //     $join->on('kartu_stok_dompuls.id_produk','=','upload_dompuls.produk');
+//                         // })
+//                         ->whereBetween('tanggal_transaksi',[$tgl_awal,$tgl_akhir])
+//                         ->groupBy('nama');
         $stokDompul = DB::table('kartu_stok_dompuls')->select(DB::raw("kartu_stok_dompuls.id_produk as nama,
-        (SELECT sum(awal.masuk)-sum(awal.keluar)
-FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi < '{$tgl_awal}' AND awal.id_produk=nama) AS stok_awal,
-sum(kartu_stok_dompuls.masuk) AS stok_masuk, sum(kartu_stok_dompuls.keluar) AS stok_keluar,
-(sum(kartu_stok_dompuls.masuk)-sum(kartu_stok_dompuls.keluar)+COALESCE((SELECT sum(awal.masuk)-sum(awal.keluar)
-FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi < '{$tgl_awal}' AND awal.id_produk=nama),0)) AS jumlah_stok"))
-                        // ->join('upload_dompuls',function($join){
-                        //     $join->on('kartu_stok_dompuls.id_produk','=','upload_dompuls.produk');
-                        // })
-                        ->whereBetween('tanggal_transaksi',[$tgl_awal,$tgl_akhir])
+        COALESCE((SELECT sum(awal.masuk)-sum(awal.keluar)
+FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi < '{$tgl_awal}' AND awal.id_produk=nama),0) AS stok_awal,
+COALESCE((SELECT sum(awal.masuk)
+FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi BETWEEN '{$tgl_awal}' AND '{$tgl_akhir}' AND awal.id_produk=nama),0) AS stok_masuk,
+COALESCE((SELECT sum(awal.keluar)
+FROM kartu_stok_dompuls awal WHERE awal.tanggal_transaksi BETWEEN '{$tgl_awal}' AND '{$tgl_akhir}' AND awal.id_produk=nama),0) AS stok_keluar,
+(sum(masuk)-sum(keluar)) AS jumlah_stok"))
+                        ->whereRaw("tanggal_transaksi < '{$tgl_akhir}'")
                         ->groupBy('nama');
-
         $dompuls = UploadDompul::select('produk','stok_awal','stok_masuk','stok_keluar','jumlah_stok')
         ->leftJoinSub($stokDompul, 'total_nominal', function($join) {
                             $join->on('upload_dompuls.produk', '=', 'total_nominal.nama');
