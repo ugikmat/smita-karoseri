dafe6061fa288b786f4708e2f35e527f9ea1ff1c
andichapras <prassmile.ps@gmail.com>
2018-09-11 21:55:12 +0700

    change button locate in master user

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -57,11 +57,6 @@
                 <div class="navbar-custom-menu">
 
                     <ul class="nav navbar-nav">
-                      <li>
-                        <a href="/tambah_user/add-user">
-                          <i class="fa fa-user-plus"></i> Tambah User
-                        </a>
-                      </li>
                         <li>
                             @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
                                 <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}">
