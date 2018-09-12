746bfa4d3dd426f1f658ad05ae03dcaf0730d8bc
andichapras <prassmile.ps@gmail.com>
2018-09-12 14:22:48 +0700

    finishing user edit page

--- resources/views/user/user-edit.blade.php
+++ resources/views/user/user-edit.blade.php
@@ -1,5 +1,5 @@
 @extends('adminlte::page') @section('title', 'User') @section('content_header')
-<h1>Daftar User</h1>
+<h1>Edit User</h1>
 
 @stop @section('css')
 <style>
@@ -15,103 +15,87 @@
     <div id="deleted">
 
   </div>
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nama
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="name" name="name" class="form-control col-md-7 col-xs-12" required="required" value="{{$data->name}}">
-        </div>
+
+  <div class="container-fluid form-inline">
+    <div class="row">
+      <div class="col-xs-6 col-sm-6">
+        Nama&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;:&emsp;
+        <input type="text" id="name" name="name" class="form-control" value="{{$data->name}}" autocomplete="off" required>
+      </div>
     </div>
 
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Username
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="text" id="username" name="username" class="form-control col-md-7 col-xs-12" required="required" value="{{$data->username}}">
-        </div>
+  <br>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6">
+      Username&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;:&emsp;
+      <input type="text" id="username" name="username" class="form-control" value="{{$data->username}}" autocomplete="off" required>
     </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6">
+      Password baru&emsp;&emsp;&emsp;&emsp;:&emsp;
+      <input type="password" id="password" name="password" class="form-control" value="" autocomplete="off">
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6">
+      Konfirmasi Password&emsp;&nbsp;:&emsp;
+      <input type="password" id="konfirmasi" name="konfirmasi" class="form-control" value="" autocomplete="off">
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6">
+      Email&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;:&emsp;
+      <input type="email" id="email" name="email" class="form-control" value="{{$data->email}}" autocomplete="off" required>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6">
+      BO&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;:&emsp;
+      <select class="" name="bo" id="bo" style="height: calc(3.5rem - 2px); width:177px;" required>
+        @isset($bosarray) @foreach ($bosarray as $value)
+        <option value="{{$value->id_bo}}">{{$value->nama_bo}}</option>
+        @endforeach @endisset
+      </select>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-xs-6 col-sm-6">
+      Level User&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;:&emsp;
+      <select class="" name="level" id="level" style="height: calc(3.5rem - 2px); width:177px;" required value="{{$data->level_user}}">
+        <option value="Canvaser">Canvaser</option>
+        <option value="Kasir">Kasir</option>
+        <option value="Supervisor">Supervisor</option>
+        <option value="Kepala Cabang">Kepala Cabang</option>
+        <option value="Keuangan">Super Admin</option>
+      </select>
 
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Password Baru
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="password" id="password" name="password" class="form-control col-md-7 col-xs-12" value="">
-        </div>
     </div>
-
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Konfirmasi Password
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="password" id="konfirmasi" name="konfirmasi" class="form-control col-md-7 col-xs-12" value="">
+  </div>
+  <br>
+  <div data-repeater-list="lokasi-user" id="lokasi-user">
+    <div data-repeater-item>
+      <div class="row">
+        <div class="col-xs-6 col-sm-6">
+          Lokasi&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;:&emsp;
+          <select name="lokasi" required class="form-control" id="lokasi" style="height: calc(3.5rem - 2px); width:177px;">
+              @isset($lokasiarray) @foreach($lokasiarray as $lokasi)
+              <option value="{{$lokasi->id_lokasi}}" id="{{$lokasi->nm_lokasi}}">{{$lokasi->nm_lokasi}}</option>
+              @endforeach @endisset
+          </select>
+          <button data-repeater-delete type="button" class="btn btn-danger"> <span class="glyphicon glyphicon-remove"></span> Delete</button>
         </div>
+      </div>
+      <br>
     </div>
-
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Email
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <input type="email" id="email" name="email" class="form-control col-md-7 col-xs-12" required="required" value="{{$data->email}}">
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">BO
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <select id="bo" required="required" name="bo" placeholder="Pilih Level User" class="form-control col-md-7 col-xs-12">
-                @isset($bosarray) @foreach ($bosarray as $value)
-                <option value="{{$value->id_bo}}">{{$value->nama_bo}}</option>
-                @endforeach @endisset
-            </select>
-        </div>
-    </div>
-
-    <div class="form-group row">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Level User
-            <span class="required">*</span>
-        </label>
-        <div class="col-md-6 col-sm-6 col-xs-12">
-            <select id="level" required="required" name="level" placeholder="Pilih Level User" class="form-control col-md-7 col-xs-12" value="{{$data->level_user}}">
-                <option value="Canvaser">Canvaser</option>
-                <option value="Kasir">Kasir</option>
-                <option value="Supervisor">Supervisor</option>
-                <option value="Kepala Cabang">Kepala Cabang</option>
-                <option value="Keuangan">Keuangan</option>
-                <option value="Super Admin">Super Admin</option>
-            </select>
-        </div>
-    </div>
-
-    <br>
-    <div class='repeater'>
-        <div data-repeater-list="lokasi-user" id="lokasi-user">
-            <div data-repeater-item>
-                <div class="form-group row">
-                    <input type="hidden" id="id_users_lokasi" name="id_users_lokasi" class="form-control" value="">
-                    <div class="col-xs-6 col-sm-6">
-                        Lokasi&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;:&emsp;
-                        <select name="lokasi" required class="form-control" id="lokasi" style="height: calc(3.5rem - 2px); width:177px;">
-                            @isset($lokasiarray) @foreach($lokasiarray as $lokasi)
-                            <option value="{{$lokasi->id_lokasi}}" id="{{$lokasi->nm_lokasi}}">{{$lokasi->nm_lokasi}}</option>
-                            @endforeach @endisset
-                        </select>
-                        <button data-repeater-delete type="button" class="btn btn-danger">
-                            <span class="glyphicon glyphicon-remove"></span> Delete</button>
-                    </div>
-                </div>
-                <br>
-            </div>
-        </div>
-        <button data-repeater-create type="button" class="btn btn-warning">
-            <span class="glyphicon glyphicon-plus"></span> Tambah Lokasi</button>
-    </div>
+  </div>
+<button data-repeater-create type="button" class="btn btn-warning"> <span class="glyphicon glyphicon-plus"></span> Tambah Lokasi</button>
+  </div>
 
     <div class="ln_solid"></div>
     <div class="form-group">
