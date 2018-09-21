451a2742ec1e4d7ed2289797bdb13ec9cc8caf26
andichapras <prassmile.ps@gmail.com>
2018-09-21 16:28:23 +0700

    laporan pembelian

--- resources/views/pembelian/laporan-pembelian/Lbeli-dompul-2.blade.php
+++ resources/views/pembelian/laporan-pembelian/Lbeli-dompul-2.blade.php
@@ -83,7 +83,7 @@
             serverSide: true,
             processing: true,
             stateSave: true,
-            ajax: '/laporan-penjualan/piutang/'+id+'/'+tgl,
+            ajax: '/operasional/smita/laporan-penjualan/piutang/'+id+'/'+tgl,
             "columnDefs": [ {
             "searchable": false,
             "orderable": false,
