a0b99a87f7db247d067c2480c9d3bb941d302659
andichapras <prassmile.ps@gmail.com>
2018-09-22 11:25:34 +0700

    edit list invoice sp and mutasi sp all cvs

--- resources/views/persediaan/mutasi-sp-semua-cvs.blade.php
+++ resources/views/persediaan/mutasi-sp-semua-cvs.blade.php
@@ -90,6 +90,7 @@
             serverSide: true,
             processing: true,
             stateSave: true,
+            scrollX: true,
             lengthMenu: [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ],
             ajax: `/stok-sp/all/data/${$tgl_awal}/${$tgl_akhir}`,
             columns: [
